2e5078949ef01e3a92d675858ff205eee2b2986c
humy06001 <humy06001@E-PW0234KS>
2023-10-04 12:27:15 +0200

    Add project files.

---
+++ Kap 3 Uppgift 3.7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Uppgift3_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv en addition, subtraktion, multiplikation eller division med två tal:");
+            string calc = Console.ReadLine();
+
+            string sign = calc.Contains("+") ? "+" : (calc.Contains("-") ? "-" : (calc.Contains("*") ? "*" : "/")); //Vilket räknesätt?
+
+            int signIndex = calc.IndexOf(sign); //Index för sign
+            double number1 = double.Parse(calc[..signIndex]);  //Tal 1
+            double number2 = double.Parse(calc[(signIndex + 1)..]);  //Tal 2
+
+            if (sign == "+") //Plus
+            {
+                Console.WriteLine($"{calc} = {number1 + number2}");
+            }
+            else if (sign  == "-") //Minus
+            {
+                Console.WriteLine($"{calc} = {number1 - number2}");
+            }
+            else if (sign == "*") //Gånger
+            {
+                Console.WriteLine($"{calc} = {number1 * number2}");
+            }
+            else //Delat med
+            {
+                Console.WriteLine($"{calc} = {number1 / number2}");
+            }
+        }
+    }
+}
